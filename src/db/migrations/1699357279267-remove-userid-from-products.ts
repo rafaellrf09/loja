4075a61051a28912ced7e0215cce4b89ac13d3b9
Rafael Figueiredo <rafaellrf09@gmail.com>
2023-11-07 08:44:17 -0300

    feat: create migrations

---
+++ src/db/migrations/1699357279267-remove-userid-from-products.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class removeUseridFromProducts1699357279267 implements MigrationInterface {
+    name = 'removeUseridFromProducts1699357279267'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "produtos" DROP COLUMN "usuario_id"`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "produtos" ADD "usuario_id" character varying(100) NOT NULL`);
+    }
+
+}
