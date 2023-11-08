3fc59422a3d49e395e959399de895e948861240d
Rafael Figueiredo <rafaellrf09@gmail.com>
2023-11-08 12:02:32 -0300

    refactor: create a filter for all errors

--- src/filtros/filtro-de-excecao-https.ts
+++ src/filtros/filtro-de-excecao-https.ts
@@ -1,17 +1,27 @@
-import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from "@nestjs/common";
-import { Response } from "express";
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from "@nestjs/common";
+import { Response, Request } from "express";
 
-@Catch(HttpException)
-export class FiltroDeExcecaoHttp implements ExceptionFilter{
-    catch(excecao: HttpException, host: ArgumentsHost) {
+@Catch()
+export class FiltroDeExcecaoHttp implements ExceptionFilter {
+    catch(excecao: unknown, host: ArgumentsHost) {
         console.log(excecao);
 
         const contexto = host.switchToHttp();
         const resposta = contexto.getResponse<Response>();
+        const requisicao = contexto.getRequest<Request>();
 
-        const status = excecao.getStatus();
-
-        const body = excecao.getResponse();
+        const { status, body } = excecao instanceof HttpException ?
+            {
+                status: excecao.getStatus(),
+                body: excecao.getResponse(),
+            } : {
+                status: HttpStatus.INTERNAL_SERVER_ERROR,
+                body: {
+                    statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+                    timestamp: new Date().toISOString(),
+                    path: requisicao.url,
+                }
+            }
 
         resposta.status(status).json(body);
     }
