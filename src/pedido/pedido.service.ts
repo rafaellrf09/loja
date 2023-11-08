e1b1babbba9660468aabf95a31fde883af3056d0
Rafael Figueiredo <rafaellrf09@gmail.com>
2023-11-08 11:39:18 -0300

    feat: add error when user send quantity biggest

--- src/pedido/pedido.service.ts
+++ src/pedido/pedido.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { UpdatePedidoDto } from './dto/update-pedido.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PedidoEntity } from './pedido.entity';
@@ -20,38 +20,52 @@ export class PedidoService {
     private readonly produtoRepository: Repository<ProdutoEntity>,
   ) { }
 
-  private async buscaUsuario(id: string){
-    const usuario = await this.usuarioRepository.findOneBy({ id});
+  private async buscaUsuario(id: string) {
+    const usuario = await this.usuarioRepository.findOneBy({ id });
     if (!usuario) throw new NotFoundException('Usuário não encontrado');
     return usuario;
+  }
+
+  private trataDadosDoPedido(dadosDoPedido: CriaPedidoDTO, produtosRelacionados: ProdutoEntity[]) {
+    dadosDoPedido.itensPedido.forEach(itemPedido => {
+      const produtoRelacionado = produtosRelacionados.find(produto => produto.id === itemPedido.produtoId);
+      if (!produtoRelacionado)
+        throw new NotFoundException(`O produto com id: ${itemPedido.produtoId} não foi encontrado`);
+
+      if (itemPedido.quantidade > produtoRelacionado.quantidadeDisponivel)
+        throw new BadRequestException(`a quantidade solicidada: (${itemPedido.quantidade}) é maior que a disponível: (${produtoRelacionado.quantidadeDisponivel}) do produto '${produtoRelacionado.nome}'`);
+    });
+
   }
 
   async cadastraPedido(usuarioId: string, dadosDoPedido: CriaPedidoDTO) {
     const usuario = await this.buscaUsuario(usuarioId);
     const produtosIds = dadosDoPedido.itensPedido.map(itemPedido => itemPedido.produtoId);
 
-    const produtosRelacionados = await this.produtoRepository.findBy({id: In(produtosIds)});
+    const produtosRelacionados = await this.produtoRepository.findBy({ id: In(produtosIds) });
 
     const pedidoEntity = new PedidoEntity();
+
+    this.trataDadosDoPedido(dadosDoPedido, produtosRelacionados);
 
     const itemsPedidoEntidades = dadosDoPedido.itensPedido.map(itemPedido => {
       const produtoRelacionado = produtosRelacionados.find(produto => produto.id === itemPedido.produtoId);
 
-      if (!produtoRelacionado) throw new NotFoundException(`O produto com idd: ${itemPedido.produtoId} não foi encontrado`);
-      
+
+
       const itemPedidoEntity = new ItemPedidoEntity();
-      
-      itemPedidoEntity.produto = produtoRelacionado;
-      itemPedidoEntity.precoVenda = produtoRelacionado.valor;
+
+      itemPedidoEntity.produto = produtoRelacionado!;
+      itemPedidoEntity.precoVenda = produtoRelacionado!.valor;
       itemPedidoEntity.quantidade = itemPedido.quantidade;
       itemPedidoEntity.produto.quantidadeDisponivel -= itemPedido.quantidade;
 
       return itemPedidoEntity;
     });
-    
+
     const valorTotal = itemsPedidoEntidades.reduce((total, item) => {
-    return total + item.precoVenda * item.quantidade
-  }, 0);
+      return total + item.precoVenda * item.quantidade
+    }, 0);
 
     pedidoEntity.status = StatusPedido.EM_PROCESSAMENTO;
     pedidoEntity.usuario = usuario;
