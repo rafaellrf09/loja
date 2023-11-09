41cb36b1b6aae4797a6cffea31d91603069ace80
Rafael Figueiredo <rafaellrf09@gmail.com>
2023-11-09 16:25:50 -0300

    feat: Add JWT auth

--- src/modulos/auth/auth.module.ts
+++ src/modulos/auth/auth.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsuarioModule } from '../usuario/usuario.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports:[UsuarioModule],
+  imports: [UsuarioModule, JwtModule.register({
+    global: true,
+    secret: "SEGREDO",
+    signOptions: { expiresIn: "72h", },
+  }),],
   controllers: [AuthController],
   providers: [AuthService],
 })
-export class AuthModule {}
+export class AuthModule { }
--- src/modulos/auth/auth.service.ts
+++ src/modulos/auth/auth.service.ts
@@ -1,16 +1,32 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsuarioService } from '../usuario/usuario.service';
 import * as bcrypt from "bcrypt";
+import { JwtService } from '@nestjs/jwt';
+
+interface UsuarioPayloadJWT {
+  sub: string,
+  nomeUsuario: string
+}
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usuarioService: UsuarioService) { }
+  constructor(
+    private readonly usuarioService: UsuarioService,
+    private readonly jwtService: JwtService
+  ) { }
   async login(email: string, senha: string) {
     const usuario = await this.usuarioService.buscaPorEmail(email);
 
     const usuarioFoiAutenticado = await bcrypt.compare(senha, usuario.senha);
-    if(!usuarioFoiAutenticado) throw new UnauthorizedException("O email ou a senha está incorreto");
+    if (!usuarioFoiAutenticado) throw new UnauthorizedException("O email ou a senha está incorreto");
 
-    return usuarioFoiAutenticado;
+    const payload: UsuarioPayloadJWT = {
+      sub: usuario.id,
+      nomeUsuario: usuario.nome,
+    }
+
+    return {
+      token: await this.jwtService.signAsync(payload),
+    };
   }
 }
