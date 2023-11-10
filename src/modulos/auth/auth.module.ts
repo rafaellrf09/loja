af7c8eff5a97495de7d6302df6ec9d9ca82b9a8f
Rafael Figueiredo <rafaellrf09@gmail.com>
2023-11-10 10:09:36 -0300

    feat: add jwt secret on .env

--- src/modulos/auth/auth.module.ts
+++ src/modulos/auth/auth.module.ts
@@ -3,12 +3,16 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsuarioModule } from '../usuario/usuario.module';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsuarioModule, JwtModule.register({
+  imports: [UsuarioModule, JwtModule.registerAsync({
+    useFactory: (configService: ConfigService) => ({
+      secret: configService.get<string>("JWT_SECRET"),
+      signOptions: { expiresIn: "72h", },
+    }),
+    inject: [ConfigService],
     global: true,
-    secret: "SEGREDO",
-    signOptions: { expiresIn: "72h", },
   }),],
   controllers: [AuthController],
   providers: [AuthService],
